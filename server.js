8a61d6dd9916bc4251fad6a4d43bab0d0ee07a67
jojun0 <jojun01835@gmail.com>
2023-08-07 16:56:49 +0900

    password

--- server.js
+++ server.js
@@ -16,21 +16,12 @@ const upload = multer({
 });
 
 app.use(express.json());
-app.use(cors()); //브라우저의 cors 이슈를 막기 위해 사용하는 코드
+app.use(cors()); // 브라우저의 CORS 이슈를 막기 위해 사용하는 코드
 app.use("/uploads", express.static("uploads"));
+
 app.get("/products", (req, res) => {
-  /*  const query=req.query;
-    console.log('queryString:', query)
-    res.send({
-        products: [
-            {id:0, name : "독버섯조명", price:89000, seller : "유니스", imageUrl:"img/products/product01.jpg"},
-            {id:1, name : "허리아작의자", price:91000, seller : "더휴먼", imageUrl:"img/products/product02.jpg"},
-            {id:2, name : "잘록거울", price:180000, seller : "더부엌", imageUrl:"img/products/product03.jpg"}
-        ]
-    }) */
   models.Product.findAll({
-    /* limit:1 */
-    order: [["createdAt", "DESC"] /* 오름차순: ASC,  내림차순: DESC */],
+    order: [["createdAt", "DESC"]],
     attributes: ["id", "name", "price", "seller", "imageUrl", "createdAt", "soldout"],
   })
     .then((result) => {
@@ -87,6 +78,7 @@ app.get("/products/:id", (req, res) => {
       res.status(400).send("상품조회가 에러가 발생함");
     });
 });
+
 app.post("/purchase/:id", (req, res) => {
   const { id } = req.params;
   models.Product.update(
@@ -130,12 +122,36 @@ app.delete("/products/:id", (req, res) => {
     });
 });
 
+const passwords = {
+  // 임의의 제품 ID와 비밀번호를 매핑한 데이터
+  1: "1592",
+  // ... 추가적인 제품 ID와 비밀번호 매핑 데이터
+};
+
+app.post("/products/:id/verify-password", (req, res) => {
+  const { id } = req.params;
+  const { password } = req.body;
+
+  // 제품 ID에 해당하는 비밀번호를 데이터베이스에서 가져옵니다.
+  const correctPassword = passwords[id];
+
+  // 입력받은 비밀번호와 저장된 비밀번호를 비교하여 검증합니다.
+  if (password === correctPassword) {
+    // 비밀번호가 일치하는 경우 200 상태코드와 메시지를 응답합니다.
+    res.sendStatus(200);
+  } else {
+    // 비밀번호가 일치하지 않는 경우 403 상태코드와 메시지를 응답합니다.
+    res.sendStatus(403);
+  }
+});
+
 app.post("/image", upload.single("image"), (req, res) => {
   const file = req.file;
   res.send({
     imageUrl: file.path,
   });
 });
+
 app.listen(port, () => {
   models.sequelize
     .sync()
